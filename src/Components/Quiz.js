b69fca18c489da27ed1e9c68dc2770e24e511404
yl0517 <yl0517@uw.edu>
2024-05-13 17:04:01 -0700

    quiz updated

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -8,27 +8,33 @@ function Quiz({ data }) {
     const [activeQuestion, setActiveQuestion] = useState(0);
     const [selectedAnswer, setSelectedAnswer] = useState('');
     const [result, setResult] = useState({
-        correctAnswers: 0,
-        wrongAnswers: 0
+        goodwill: 0,
+        second: 0,
+        vintage: 0,
+        consignment: 0
     });
     const [showResult, setShowResult] = useState(false);
     const [selectedAnswerIdx, setSelectedAnswerIdx] = useState(null);
     const [started, setStarted] = useState(false);
 
     const questions = data.quiz;
-    const { question, answers, correct_answer } = questions[activeQuestion];
+    const currentQuestion = questions[activeQuestion];
+    const question = currentQuestion ? currentQuestion.question : '';
+    const answers = currentQuestion ? currentQuestion.answers : [];
 
 
-
-    function handleClick() {
+    function handleNextClick() {
         setResult((prev) => {
             let newResult = { ...prev };
 
-            if (selectedAnswer) {
-                newResult.correctAnswers = prev.correctAnswers + 1;
-            } else {
-                newResult.wrongAnswers = prev.wrongAnswers + 1;
-
+            if (selectedAnswer.key === 0) {
+                newResult.goodwill = prev.goodwill + 1;
+            } else if (selectedAnswer.key === 1) {
+                newResult.second = prev.second + 1;
+            } else if (selectedAnswer.key === 2) {
+                newResult.vintage = prev.vintage + 1;
+            } else if (selectedAnswer.key === 3) {
+                newResult.consignment = prev.consignment + 1;
             }
             return newResult;
         });
@@ -40,15 +46,25 @@ function Quiz({ data }) {
             setActiveQuestion((prev) => prev + 1)
         }
         setSelectedAnswerIdx(null);
-        setSelectedAnswer(false);
+        setSelectedAnswer('');
     }
+
+    function handlePrevClick() {
+        setActiveQuestion((prev) => prev - 1);
+        const previousAnswer = questions[activeQuestion - 1].selectedAnswer;
+        setSelectedAnswerIdx(previousAnswer.index);
+        setSelectedAnswer(previousAnswer);
+    }
+
 
     function handleAnswerSelected(answer, index) {
         return () => {
             setSelectedAnswerIdx(index);
-            setSelectedAnswer(answer === correct_answer);
+            setSelectedAnswer({ text: answer, key: index });
+            questions[activeQuestion].selectedAnswer = { text: answer, index: index };
         }
     }
+
 
     function getClassName(index) {
         if (selectedAnswerIdx === index) {
@@ -95,11 +111,11 @@ function Quiz({ data }) {
                     <h2>{question}</h2>
                     <ul>
                         {answers.map((answer, index) => (
-
                             <li key={answer} onClick={handleAnswerSelected(answer, index)} className={getClassName(index)}>{answer}</li>
                         ))}
                     </ul>
-                    <button onClick={handleClick} disabled={selectedAnswerIdx === null}>{changeButtonStatus()}</button>
+                    <button onClick={handlePrevClick} disabled={activeQuestion === 0}>Prev</button>
+                    <button onClick={handleNextClick} disabled={selectedAnswerIdx === null}>{changeButtonStatus()}</button>
                 </div>
             )
         } else {
@@ -107,15 +123,58 @@ function Quiz({ data }) {
                 <div className="quiz-result-container">
                     <h1>Result</h1>
                     <p>
-                        Total Question: <span>{questions.length}</span>
-                    </p>
-                    <p>
-                        Correct Answers:<span> {result.correctAnswers}</span>
-                    </p>
-                    <p>
-                        Wrong Answers:<span> {result.wrongAnswers}</span>
+                        Your Best Fit: <span>{getBestFit()}</span>
                     </p>
                     <button onClick={handleRestart}>Restart Quiz</button>
+                </div>
+            )
+        }
+    }
+
+    function getBestFit() {
+        const highest = Object.keys(result).reduce((a, b) => result[a] > result[b] ? a : b);
+        if (highest === "goodwill") {
+            return (
+                <div>
+                    <h2>Goodwill Bins</h2>
+                    <ul>
+                        <li>Likes the process of finding something</li>
+                        <li>People that have experience with thrifting </li>
+                        <li>Doesn’t mind putting in more work </li>
+                        <li>Cheapest option, most time consuming</li>
+                    </ul>
+                </div>
+            )
+        } else if (highest === "second") {
+            return (
+                <div>
+                    <h2>Second-Hand</h2>
+                    <ul>
+                        <li>Good for beginners</li>
+                        <li>People that want affordable options</li>
+                        <li>People that want a traditional store experience, looking thought racks, organized</li>
+                    </ul>
+                </div>
+            )
+        } else if (highest === "vintage") {
+            return (
+                <div>
+                    <h2>Vintage</h2>
+                    <ul>
+                        <li>People interested in finding unique, historical pieces</li>
+                        <li>Sometimes expensive, sometimes not</li>
+                    </ul>
+                </div>
+            )
+        } else if (highest === "consignment") {
+            return (
+                <div>
+                    <h2>Consignment</h2>
+                    <ul>
+                        <li>People looking for curated pieces</li>
+                        <li>Usually more modern</li>
+                        <li>Relatively More Expensive</li>
+                    </ul>
                 </div>
             )
         }
@@ -129,8 +188,10 @@ function Quiz({ data }) {
         setActiveQuestion(0);
         setSelectedAnswer('');
         setResult({
-            correctAnswers: 0,
-            wrongAnswers: 0
+            goodwill: 0,
+            second: 0,
+            vintage: 0,
+            consignment: 0
         })
         setShowResult(false);
         setSelectedAnswerIdx(null)
