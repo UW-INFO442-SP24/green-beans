437f5e753ba9593ea472368009e3235c9a0df332
yl0517 <yl0517@uw.edu>
2024-05-15 14:59:06 -0700

    navbar changed

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,18 +4,31 @@ import logo from '../images/logo.png';
 
 function Navbar() {
     return (
-        <div>
-            <nav>
-                <ul className="nav">
-                    <li className="nav-logo"><Link className="text-decoration-none" to="/" ><img src={logo} alt="logo" /></Link></li>
-                    <li className="nav-item"><Link className="text-decoration-none" to="/events" >EVENTS</Link></li>
-                    <li className="nav-item"><Link className="text-decoration-none" to="/stores" >STORES</Link></li>
-                    <li className="nav-item"><Link className="text-decoration-none" to="/quiz" >QUIZ</Link></li>
-                    <li className="nav-item"><Link className="text-decoration-none" to="/about" >ABOUT</Link></li>
+        <nav className="navbar navbar-expand-sm navbar-light">
+            <Link className="navbar-brand" to="/">
+                <img src={logo} alt="logo" style={{ maxHeight: '60px' }} />
+            </Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/events">EVENTS</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/stores">STORES</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/quiz">QUIZ</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/about">ABOUT</Link>
+                    </li>
                 </ul>
-            </nav>
-        </div>
-    )
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
