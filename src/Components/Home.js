b5346c3b9856dd38e34bbb5d9e2e248504040784
tiffylu <tlu20@uw.edu>
2024-05-29 14:38:52 -0700

    added img alt descriptions for home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,7 +13,7 @@ function Home() {
         <div className="home">
             <div className="home-quiz">
                 <div className="home-quiz-without-arrow">
-                    <img src={quizImg} alt="quizImg" />
+                    <img src={quizImg} alt="aesthetic purple and green clothing" />
                     <div className="home-quiz-content">
                         <h1>DISCOVER YOUR ECO FRIENDLY STYLE</h1>
                         <button onClick={() => navigate("/quiz")}>TAKE THE QUIZ</button>
@@ -24,7 +24,7 @@ function Home() {
             <div className="home-stores">
                 <h1>STORES</h1>
                 <div className="home-stores-without-title">
-                    <img src={storesImg} alt="storesImg" />
+                    <img src={storesImg} alt="a vintage thrift shop" />
                     <div className="home-stores-content">
                         <p>Discover Seattle's sustainable fashion gems with our curated list of thrift and vintage stores! </p>
                         <button onClick={() => navigate("/stores")}>EXPLORE STORES</button>
@@ -34,7 +34,7 @@ function Home() {
             <div className="home-events">
                 <h1>EVENTS</h1>
                 <div className="home-events-without-title">
-                    <img src={eventsImg} alt="eventsImg" />
+                    <img src={eventsImg} alt="a swap party event" />
                     <div className="home-events-content">
                         <p>Dive into unique events and experiences in Seattle that support local businesses and promote eco-friendly living!</p>
                         <button onClick={() => navigate("/events")}>EXPLORE EVENTS</button>
@@ -42,7 +42,7 @@ function Home() {
                 </div>
             </div>
             <div className="home-about">
-                <img src={aboutImg} alt="eventsImg" />
+                <img src={aboutImg} alt="a model with clothing over her head" />
                 <div className="home-about-content">
                     <h1>ABOUT US</h1>
                     <p>Explore the impacts of fast fashion and uncover the benefits of sustainable shopping.</p>
